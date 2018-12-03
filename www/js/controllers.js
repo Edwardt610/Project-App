dff681c1c86de9ca50269e68a0806f312c82d08e
Edwardt610 <mingyeung610@gmail.com>
2018-12-03 10:30:34 +0800

    Finsih Highlight Event Page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,19 @@
 angular.module('app.controllers', [])
-  
+
 .controller('highlightEventsCtrl', ['$scope', '$stateParams', '$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $http) {
+  $scope.$on("$ionicView.beforeEnter", function() {
     $http.get("http://localhost:1337/").then(function(response){
         console.log(response.data);
+        $scope.items = response.data.events;
+        //console.log($scope.item[2].imgURL);
     });
+  });
 
 }])
-   
+
 .controller('searchByDepartmentCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -17,7 +21,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('searchByVenueCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -25,15 +29,46 @@ function ($scope, $stateParams) {
 
 
 }])
-      
-.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('loginCtrl', ['$scope', '$stateParams', '$http', '$ionicHistory', '$ionicPopup','$state',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $http, $ionicHistory, $ionicPopup, $state) {
 
+    $scope.data = {};
+
+    $scope.login = function () {
+
+        $http.post("http://localhost:1337/user/login", $scope.data)
+        .then(function (response) {
+
+            // A confirm dialog
+            var confirmPopup = $ionicPopup.confirm({
+                title: 'Welcome back!',
+                template: 'Go back to previous page?'
+            });
+
+            confirmPopup.then(function (res) {
+                if (res) {
+                    $ionicHistory.goBack();
+                } else {
+                    console.log('granted');
+                    $state.go($state.current, {}, { reload: true });
+                }
+            });
+
+        }, function (response) {
+
+            var alertPopup = $ionicPopup.alert({
+                title: response.data,
+                template: 'Login failed. Please try again.'
+            });
+        });
+
+    }
 
 }])
-   
+
 .controller('accountInfoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -41,7 +76,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('departmentEventsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -49,7 +84,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('venueEventsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -57,7 +92,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('eventDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -65,7 +100,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('registeredEventCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -73,4 +108,3 @@ function ($scope, $stateParams) {
 
 
 }])
- 
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,34 @@
 angular.module('app.services', [])
+.service('Management',function() {
+
+  this.getDepartmentEvents = function (sDepartment) {
+      return department_events.filter(function (event) {
+          return event.department === sDepartment;
+      });
+  };
+
+  this.getVenueEvents = function (sVenue) {
+      return venue_events.filter(function (event) {
+          return event.department === sVenue;
+      });
+  };
+
+  this.getSelected = function() {
+
+      return fashion_items.filter(function(item) {
+          return item.quantity > 0;
+      });
+
+  };
+
+  this.getItem = function (id) {
+
+      return fashion_items.filter(function (item) {
+          return item.id === id;
+      })[0] || null;
+
+  };
+})
 
 .factory('BlankFactory', [function(){
 
